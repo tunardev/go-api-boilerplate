67cf295f863ba6b737d0e28f14c72dd32336c651
tunardev <tunardev@gmail.com>
2023-02-20 00:10:59 +0400

    add: errors package

--- interval/story/api.go
+++ interval/story/api.go
@@ -3,6 +3,7 @@ package story
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/tunardev/go-api-boilerplate/interval/models"
+	"github.com/tunardev/go-api-boilerplate/pkg/errors"
 )
 
 type routes struct {
@@ -16,26 +17,18 @@ func (r routes) create(c *fiber.Ctx) error {
 	// Parse the request body into the story.
 	err := c.BodyParser(&story)
 	if err != nil {
-		return c.Status(400).JSON(fiber.Map{
-			"error": err.Error(),
-			"status": 400,
-		})
+		return c.Status(400).JSON(errors.BadRequest(err.Error()))
 	}
 
+	// Validate the story.
 	if ok := story.Validate(); !ok {
-		return c.Status(400).JSON(fiber.Map{
-			"error": "invalid story",
-			"status": 400,
-		})
+		return c.Status(400).JSON(errors.BadRequest("invalid story"))
 	}
 
 	// Create the story in the database.
 	story, err = r.service.Create(story)
 	if err != nil {
-		return c.Status(500).JSON(fiber.Map{
-			"error": err.Error(),
-			"status": 500,
-		})
+		return c.Status(500).JSON(errors.InternalServerError(err.Error()))
 	}
 
 	// Return the story.
@@ -52,10 +45,7 @@ func (r routes) get(c *fiber.Ctx) error {
 	// Get the story from the database.
 	story, err := r.service.Get(id)
 	if err != nil {
-		return c.Status(500).JSON(fiber.Map{
-			"error": err.Error(),
-			"status": 500,
-		})
+		return c.Status(500).JSON(errors.InternalServerError(err.Error()))
 	}
 
 	// Return the story.
@@ -75,26 +65,18 @@ func (r routes) update(c *fiber.Ctx) error {
 	// Parse the request body into the story.
 	err := c.BodyParser(&story)
 	if err != nil {
-		return c.Status(400).JSON(fiber.Map{
-			"error": err.Error(),
-			"status": 400,
-		})
+		return c.Status(400).JSON(errors.BadRequest(err.Error()))
 	}
 
+	// Validate the story.
 	if ok := story.Validate(); !ok {
-		return c.Status(400).JSON(fiber.Map{
-			"error": "invalid story",
-			"status": 400,
-		})
+		return c.Status(400).JSON(errors.BadRequest("invalid story"))
 	}
 
 	// Update the story in the database.
 	story, err = r.service.Update(id, story)
 	if err != nil {
-		return c.Status(500).JSON(fiber.Map{
-			"error": err.Error(),
-			"status": 500,
-		})
+		return c.Status(500).JSON(errors.InternalServerError(err.Error()))
 	}
 
 	// Return the story.
---
+++ pkg/errors/errors.go
@@ -0,0 +1,44 @@
+package errors
+
+import "net/http"
+
+// ErrorResponse is the response for an error.
+type ErrorResponse struct {
+	Status  int         `json:"status"`
+	Message string      `json:"message"`
+}
+
+// Error is required by error interface.
+func (e ErrorResponse) Error() string {
+	return e.Message
+}
+
+// StatusCode returns the HTTP status code for the error.
+func (e ErrorResponse) StatusCode() int {
+	return e.Status
+}
+
+// InternalServerError creates a new error response representing an internal server error (HTTP 500)
+func InternalServerError(msg string) ErrorResponse {
+	if msg == "" {
+		msg = "Something went wrong. Please try again later."
+	}
+
+	return ErrorResponse{
+		Status:  http.StatusInternalServerError,
+		Message: msg,
+	}
+}
+
+// BadRequest creates a new error response representing a bad request (HTTP 400)
+func BadRequest(msg string) ErrorResponse {
+	if msg == "" {
+		msg = "The request was invalid or cannot be otherwise served."
+	}
+
+	return ErrorResponse{
+		Status:  http.StatusBadRequest,
+		Message: msg,
+	}
+}
+
